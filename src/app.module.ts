ca67de284f409596445daf3fc698cab1395c1b1c
Abdul Hafizh <hafizhahm123@gmail.com>
2025-09-14 05:42:36 +0700

    init profiles

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './common/guards/jwt-auth.guards';
+import { ProfilesModule } from './modules/profiles/profiles.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, ProfilesModule],
   controllers: [AppController],
   providers: [
     AppService,
