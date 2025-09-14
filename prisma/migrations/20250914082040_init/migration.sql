9a4c6c1af729c1ec8ad9a37a2d42021f4cc0e1ed
Abdul Hafizh <hafizhahm123@gmail.com>
2025-09-14 15:21:56 +0700

    chore: refactor migration

--- prisma/migrations/20250914082040_init/migration.sql
+++ prisma/migrations/20250914082040_init/migration.sql
@@ -50,5 +50,37 @@ CREATE TABLE "public"."Tags" (
     CONSTRAINT "Tags_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "public"."PostTags" (
+    "postsId" INTEGER NOT NULL,
+    "tagsId" INTEGER NOT NULL,
+
+    CONSTRAINT "PostTags_pkey" PRIMARY KEY ("postsId","tagsId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_username_key" ON "public"."Users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "public"."Users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profiles_name_key" ON "public"."Profiles"("name");
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Profiles_UserId_key" ON "public"."Profiles"("UserId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Tags_tagName_key" ON "public"."Tags"("tagName");
+
+-- AddForeignKey
+ALTER TABLE "public"."Profiles" ADD CONSTRAINT "Profiles_UserId_fkey" FOREIGN KEY ("UserId") REFERENCES "public"."Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Posts" ADD CONSTRAINT "Posts_ProfileId_fkey" FOREIGN KEY ("ProfileId") REFERENCES "public"."Profiles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."PostTags" ADD CONSTRAINT "PostTags_postsId_fkey" FOREIGN KEY ("postsId") REFERENCES "public"."Posts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."PostTags" ADD CONSTRAINT "PostTags_tagsId_fkey" FOREIGN KEY ("tagsId") REFERENCES "public"."Tags"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
